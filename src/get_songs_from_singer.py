184ff432142900efc729cf61ea44e976e16ca178
wkj2333666 <2695071759@qq.com>
2025-07-02 17:53:14 +0800

    ruff format

--- src/get_songs_from_singer.py
+++ src/get_songs_from_singer.py
@@ -10,56 +10,60 @@ import time
 import config
 
 # %%
-sex = '女歌手'
-df_singers = pd.read_csv(rf'../data/{sex}.csv')
+sex = "女歌手"
+df_singers = pd.read_csv(rf"../data/{sex}.csv")
 df_singers
 
 # %%
 # dict_songs = dict()
 
 
-df_songs = pd.DataFrame({
-    'singer': [],
-    'song_name': [],
-    'song_link': []
-})
+df_songs = pd.DataFrame({"singer": [], "song_name": [], "song_link": []})
 tqdm.pandas()
 
+
 def get_songs(row):
-    response = requests.get(url=row.链接, headers=config.HEADERS, params={}, cookies=config.COOKIES)
-    soup = bs(response.text, 'lxml')
+    response = requests.get(
+        url=row.链接, headers=config.HEADERS, params={}, cookies=config.COOKIES
+    )
+    soup = bs(response.text, "lxml")
     # print(soup.prettify())
     # pbar = tqdm.get_lock().get_instances()[0]
     # pbar.set_description_str(f'processing {row.歌手}')
 
-    song_names = [song.text for song in soup.find_all('a', href=re.compile(r'/song\?id=\d'))]
-    song_links = [r'https://music.163.com' + song['href'] for song in soup.find_all('a', href=re.compile(r'/song\?id=\d'))]
+    song_names = [
+        song.text for song in soup.find_all("a", href=re.compile(r"/song\?id=\d"))
+    ]
+    song_links = [
+        r"https://music.163.com" + song["href"]
+        for song in soup.find_all("a", href=re.compile(r"/song\?id=\d"))
+    ]
 
     global df_songs
     df_songs = pd.concat(
         [
             df_songs,
-            pd.DataFrame({
-                'singer': [row.歌手 for _ in range(len(song_names))],
-                'song_name': song_names,
-                'song_link': song_links
-            })
+            pd.DataFrame(
+                {
+                    "singer": [row.歌手 for _ in range(len(song_names))],
+                    "song_name": song_names,
+                    "song_link": song_links,
+                }
+            ),
         ],
-        ignore_index=True
-    ) 
+        ignore_index=True,
+    )
     # return df_songs_new
     # time.sleep(2.0)
 
-    
 
 # %%
 df_singers.progress_apply(get_songs, axis=1)
 
 # %%
-df_songs.to_csv(rf'../data/{sex}歌曲.csv', header=True, index=False)
+df_songs.to_csv(rf"../data/{sex}歌曲.csv", header=True, index=False)
 
 # %%
-
 
 
 # %%
--- src/singers_crawler.py
+++ src/singers_crawler.py
@@ -7,25 +7,27 @@ from bs4 import BeautifulSoup as bs
 import config
 
 # %%
-netease_singers_url = r'https://music.163.com/discover/artist/cat?id=1002'
+netease_singers_url = r"https://music.163.com/discover/artist/cat?id=1002"
 
-netease_singers_response = requests.get(url=netease_singers_url,
-                                        params={},
-                                        headers=config.HEADERS,
-                                        cookies=config.COOKIES,
-                                        allow_redirects=True)
+netease_singers_response = requests.get(
+    url=netease_singers_url,
+    params={},
+    headers=config.HEADERS,
+    cookies=config.COOKIES,
+    allow_redirects=True,
+)
 # netease_singers_response.encoding = 'utf-8'
 netease_singers_male = netease_singers_response.text
 netease_singers_male
 
 # %%
-netease_singers_male_soup = bs(netease_singers_male, 'lxml')
+netease_singers_male_soup = bs(netease_singers_male, "lxml")
 print(netease_singers_male_soup.prettify())
 
 # %%
-singers_male = netease_singers_male_soup.find_all('a',
-                                                  href=re.compile(r'artist\?id'),
-                                                  class_='nm nm-icn f-thide s-fc0')
+singers_male = netease_singers_male_soup.find_all(
+    "a", href=re.compile(r"artist\?id"), class_="nm nm-icn f-thide s-fc0"
+)
 singers_male
 
 # %%
@@ -37,22 +39,21 @@ singers_male_names = [singer_name.text for singer_name in singers_male]
 singers_male_names
 
 # %%
-singers_male_links = [singer_link['href'].strip() for singer_link in singers_male]
+singers_male_links = [singer_link["href"].strip() for singer_link in singers_male]
 singers_male_links
 
 # %%
 len(singers_male_links) == len(singers_male_names)
 
 # %%
-df_singers_male = pd.DataFrame({
-    'singer': pd.Series(singers_male_names),
-    'singer_link': (r'https://music.163.com' + pd.Series(singers_male_links))
-})
+df_singers_male = pd.DataFrame(
+    {
+        "singer": pd.Series(singers_male_names),
+        "singer_link": (r"https://music.163.com" + pd.Series(singers_male_links)),
+    }
+)
 
 # %%
-df_singers_male.to_csv('../data/女歌手.csv', sep=',', header=True, index=False)
+df_singers_male.to_csv("../data/女歌手.csv", sep=",", header=True, index=False)
 
 # %%
-
-
-
